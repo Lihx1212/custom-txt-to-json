4450d93237076d7956bd6dbda03f75148b7bf5aa
Alex Ross <arossg17@gmail.com>
2019-04-30 11:05:56 -0400

    added February workouts

--- read.py
+++ read.py
@@ -1,14 +1,18 @@
 import json
 from dictionary import createDictionary
 
-#start with March workouts
-with open('files/March.txt') as workouts_file:
-
+#February workouts
+with open('files/February.txt') as feb_workouts_file:
     
-    d = createDictionary(workouts_file)
+    d = createDictionary(feb_workouts_file)
 
-    print('--------------new nested dictionary-------------------')
-    print(d)
+    with open("files/february.json","w") as f:
+        json.dump(d,f)
+
+#March workouts
+with open('files/March.txt') as mar_workouts_file:
+    
+    d = createDictionary(mar_workouts_file)
 
     with open("files/march.json","w") as f:
         json.dump(d,f)
