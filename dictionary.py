6b20b3c67e0709a688dbe4da89eee76cd864c6c2
Alex Ross <arossg17@gmail.com>
2019-04-30 11:20:54 -0400

    cleaning and refactoring files

--- dictionary.py
+++ dictionary.py
@@ -4,7 +4,6 @@ def createDictionary(workouts_file):
 
 	lines = workouts_file.readlines()
 	newLines = formatTxtToList(lines)
-	#print(newLines)
 	nested_dictionary = {}
 	for line in newLines.splitlines():
 		newLine = line.rstrip().split(' ')
@@ -12,5 +11,4 @@ def createDictionary(workouts_file):
 		listOfKeys = ['timeOfDay' , 'distance' , 'calories' , 'strides']
 		zipbObj = zip(listOfKeys, newLine)
 		nested_dictionary[day] =  dict(zipbObj)
-		# print(d[day])
 	return nested_dictionary
--- main.py
+++ main.py
@@ -2,25 +2,25 @@ import json
 from dictionary import createDictionary
 
 #January workouts
-with open('files/January.txt') as jan_workouts_file:
+with open('files/text/January.txt') as jan_workouts_file:
     
     d = createDictionary(jan_workouts_file)
 
-    with open("files/january.json","w") as f:
+    with open("files/json/january.json","w") as f:
         json.dump(d,f)
 
 #February workouts
-with open('files/February.txt') as feb_workouts_file:
+with open('files/text/February.txt') as feb_workouts_file:
     
     d = createDictionary(feb_workouts_file)
 
-    with open("files/february.json","w") as f:
+    with open("files/json/february.json","w") as f:
         json.dump(d,f)
 
 #March workouts
-with open('files/March.txt') as mar_workouts_file:
+with open('files/text/March.txt') as mar_workouts_file:
     
     d = createDictionary(mar_workouts_file)
 
-    with open("files/march.json","w") as f:
+    with open("files/json/march.json","w") as f:
         json.dump(d,f)
