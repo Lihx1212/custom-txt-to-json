28c2d8e0ac9fd9260576e757341837e913777a8e
Alex Ross <arossg17@gmail.com>
2019-05-18 13:48:55 -0400

    added year field

--- utils/format.py
+++ utils/format.py
@@ -9,5 +9,5 @@ def formatTxtToList(lines):
             miles_ran =  lines[index + 1].rstrip()
             calories_burned =  lines[index + 2].rstrip().replace("c","")
             total_strides =  lines[index + 3].rstrip().replace("s","")
-            newLines+=f'{month} {day} {time_of_day} {miles_ran} {calories_burned} {total_strides} \n'
+            newLines+=f'2019 {month} {day} {time_of_day} {miles_ran} {calories_burned} {total_strides} \n'
     return newLines
