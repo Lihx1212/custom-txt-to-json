13774f8bb7c1ae2f593dcfa6032eead3a095fcb0
Alex Ross <arossg17@gmail.com>
2019-05-22 11:04:41 -0400

    added time and strides per min

--- utils/format.py
+++ utils/format.py
@@ -9,5 +9,6 @@ def formatTxtToList(lines):
             miles_ran =  lines[index + 1].rstrip()
             calories_burned =  lines[index + 2].rstrip().replace("c","")
             total_strides =  lines[index + 3].rstrip().replace("s","")
-            newLines+=f'2019 {month} {day} {time_of_day} {miles_ran} {calories_burned} {total_strides} \n'
+            strides_per_min = str(round(int(total_strides)/30))
+            newLines+=f'2019 {month} {day} {time_of_day} {miles_ran} {calories_burned} {total_strides} {strides_per_min} 30 \n'
     return newLines
